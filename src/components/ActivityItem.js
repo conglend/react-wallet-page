e3a9f80dae8e923215ce3facae18a909d10b1d50
mbrainx <anhdev93@gmail.com>
2022-01-13 21:57:12 -0500

    Add unit tests for ActivityItem

--- src/components/ActivityItem.js
+++ src/components/ActivityItem.js
@@ -3,14 +3,14 @@ import React from "react";
 const ActivityItem = ({ activity }) => {
   return (
     <div className="activity-item d-flex">
-      <div className="activity-item-icon">
+      <div className="activity-item-icon" data-testid="activity-icon">
         <img src={activity.icon} alt="" />
       </div>
       <div>
-        <p className="description">
+        <p className="description" data-testid="activity-description">
           <span>{activity.user}</span> {activity.text}
         </p>
-        <p className="days-ago">{activity.day} days ago</p>
+        <p className="days-ago" data-testid="activity-date">{activity.day} days ago</p>
       </div>
     </div>
   );
---
+++ src/components/ActivityItem.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+import ActivityItem from "./ActivityItem";
+import "@testing-library/jest-dom"
+
+import { activities } from "../constants";
+
+
+const props = {
+  activity: activities[0]
+}
+
+describe("Experience Detail > Activity Item", () => {
+
+  it("Should render activity icon on the activity item", () => {
+    const { getByTestId } = render(<ActivityItem {...props} />)
+    expect(getByTestId("activity-icon")).toBeInTheDocument()
+  })
+
+
+})
